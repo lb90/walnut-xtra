325000160a6a61f856f28510d9ffdc7298b103e1
Luca Bacci <luca.bacci982@gmail.com>
2019-08-03 17:54:56 +0200

    Migliora gestione della clipboard

--- clipboard.cpp
+++ clipboard.cpp
@@ -11,24 +11,28 @@ void Clipboard::CopyToClipboard(const std::string& text) {
 	if (OpenClipboard(Window::Hwnd()) == 0) {
 		DWORD error_code = GetLastError();
 		Log::print_error_code(L"Impossibile aprire la clipboard", error_code);
-		goto cleanup;
+		return;
 	}
 	if (EmptyClipboard() == 0) {
 		DWORD error_code = GetLastError();
 		Log::print_error_code(L"Impossibile azzerare la clipboard", error_code);
-		goto cleanup;
+		CloseClipboard();
+		return;
 	}
-	movable_mem = GlobalAlloc(GMEM_MOVEABLE | GMEM_ZEROINIT, text.size());
+	movable_mem = GlobalAlloc(GMEM_MOVEABLE | GMEM_ZEROINIT, text.size() + 1);
 	if (movable_mem == NULL) {
 		DWORD error_code = GetLastError();
 		Log::print_error_code(L"Impossibile allocare memoria con GlobalAlloc", error_code);
-		goto cleanup;
+		CloseClipboard();
+		return;
 	}
 	buffer = (LPSTR) GlobalLock(movable_mem);
 	if (buffer == NULL) {
 		DWORD error_code = GetLastError();
 		Log::print_error_code(L"Impossibile aprire memoria con GlobalLock", error_code);
-		goto cleanup;
+		GlobalFree(movable_mem);
+		CloseClipboard();
+		return;
 	}
 	memcpy(buffer, text.c_str(), text.size());
 	buffer[text.size()] = 0;
@@ -37,14 +41,12 @@ void Clipboard::CopyToClipboard(const std::string& text) {
 	if (SetClipboardData(CF_TEXT, (HANDLE)movable_mem) == NULL) {
 		DWORD error_code = GetLastError();
 		Log::print_error_code(L"Impossibile impostare la clipboard", error_code);
-		goto cleanup;
+		GlobalFree(movable_mem);
+		CloseClipboard();
+		return;
 	}
 	if (CloseClipboard() == 0) {
 		DWORD error_code = GetLastError();
 		Log::print_error_code(L"Impossibile chiudere la clipboard", error_code);
-		goto cleanup;
 	}
-
-cleanup:
-	return;
 }
