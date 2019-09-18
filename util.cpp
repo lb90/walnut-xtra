95da0be8ecf3dad1343ca31758aa776ddd7f69ea
Luca Bacci <luca.bacci982@gmail.com>
2019-09-18 13:12:03 +0200

    Implementa anche conversione UTF16 a UTF8

--- util.cpp
+++ util.cpp
@@ -19,7 +19,7 @@ std::wstring utf_8_to_utf_16(const std::string& string_utf_8) {
 		Log::print_error_code(L"Errore nella conversione UTF8 -> UTF16", error_code);
 	}
 
-	std::vector<wchar_t> buffer(utf_16_length+1, 0);
+	std::vector<wchar_t> buffer(utf_16_length + 1, 0);
 	const int result = MultiByteToWideChar(
 		CP_UTF8,
 		0,
@@ -34,4 +34,46 @@ std::wstring utf_8_to_utf_16(const std::string& string_utf_8) {
 
 	std::wstring string_utf_16 = buffer.data();
 	return string_utf_16;
+}
+
+std::string utf_16_to_utf_8(const std::wstring& string_utf_16) {
+	if (string_utf_16.empty())
+		return "";
+
+	const int utf_8_length = WideCharToMultiByte(
+		CP_UTF8,
+		0,
+		string_utf_16.c_str(),
+		string_utf_16.size(),
+		NULL,
+		0,
+		NULL,
+		NULL);
+	if (utf_8_length == 0) {
+		DWORD error_code = GetLastError();
+		Log::print_error_code(L"Errore nella conversione UTF16 -> UTF8", error_code);
+	}
+
+	std::vector<char> buffer(utf_8_length + 1, 0);
+	const int result = WideCharToMultiByte(
+		CP_UTF8,
+		0,
+		string_utf_16.c_str(),
+		string_utf_16.size(),
+		buffer.data(),
+		buffer.size(),
+		NULL,
+		NULL);
+	if (result == 0) {
+		DWORD error_code = GetLastError();
+		Log::print_error_code(L"Errore nella conversione UTF16 -> UTF8", error_code);
+	}
+
+	std::string string_utf_8 = buffer.data();
+	return string_utf_8;
+}
+
+bool is_all_ascii(const std::string& string_) {
+	#error "implement me!"
+	return true;
 }
--- util.h
+++ util.h
@@ -3,4 +3,7 @@
 #include <string>
 #include <windows.h>
 
+
 std::wstring utf_8_to_utf_16(const std::string& string_utf_8);
+std::string utf_16_to_utf_8(const std::wstring& string_utf_16);
+bool is_all_ascii(const std::string& string_);
