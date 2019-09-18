60ecd46fb80ac68345531bd1ddd104f480319e1f
Luca Bacci <luca.bacci982@gmail.com>
2019-09-18 16:51:40 +0200

    Modifica applicazione di test

--- TestApplication/TestApplication.cpp
+++ TestApplication/TestApplication.cpp
@@ -9,8 +9,7 @@ void __stdcall test_hazel_set(const std::string& file_name_utf_8,
                               const std::string& mode_string,
                               int& ret);
 
-int main()
-{
+void test_attributes() {
 	std::string file_name_utf_8;
 	std::string get_ret;
 	std::string mode_string;
@@ -22,36 +21,71 @@ int main()
 
 	/* Leggi gli attributi iniziali */
 	test_hazel_get(file_name_utf_8, get_ret);
-	std::cout << "[GET] Argument: \"" << file_name_utf_8 << "\" Attributes: " << get_ret << "\n";
 
 	std::string original_attributes = get_ret;
 
 	/* Prova a settare tutti e quattro gli attributi in una sola chiamata */
 	mode_string = "rahs";
 	test_hazel_set(file_name_utf_8, mode_string, set_ret);
-	std::cout << "[SET] Argument: \"" << file_name_utf_8 << "\" ModeString: " << mode_string << " Return: " << set_ret << "\n";
 
 	/* Leggi gli attributi per verificare la precedente operazione Set */
 	test_hazel_get(file_name_utf_8, get_ret);
-	std::cout << "[GET] Argument: \"" << file_name_utf_8 << "\" Attributes: " << get_ret << "\n";
 
 	/* Prova a chiamare Set con una stringa vuota (rimuove tutti e quattro gli attributi) */
 	mode_string = "";
 	test_hazel_set(file_name_utf_8, mode_string, set_ret);
-	std::cout << "[SET] Argument: \"" << file_name_utf_8 << "\" ModeString: " << mode_string << " Return: " << set_ret << "\n";
 
 	/* Leggi gli attributi per verificare la precedente operazione Set */
 	test_hazel_get(file_name_utf_8, get_ret);
-	std::cout << "[GET] Argument: \"" << file_name_utf_8 << "\" Attributes: " << get_ret << "\n";
 
 	/* Setta gli attributi originali */
 	mode_string = original_attributes;
 	test_hazel_set(file_name_utf_8, mode_string, set_ret);
-	std::cout << "[SET] Argument: \"" << file_name_utf_8 << "\" ModeString: " << mode_string << " Return: " << set_ret << "\n";
 
 	/* Verifica con un Get */
 	test_hazel_get(file_name_utf_8, get_ret);
-	std::cout << "[GET] Argument: \"" << file_name_utf_8 << "\" Attributes: " << get_ret << "\n";
+}
+
+void test_file_locking() {
+	std::string file_name;
+	std::string get;
+	std::string mode;
+	int set = 0;
+
+	test_hazel_get("C:\\Windows\\System32\\Kernel32.dll", get);
+
+	test_hazel_set("C:\\Users\\luca9\\testo.txt", "rhl", set);
+
+	test_hazel_get("C:\\Users\\luca9\\testo.txt", get);
+
+	test_hazel_set("C:\\Users\\luca9\\testo.txt", "wvs", set);
+
+	test_hazel_get("C:\\Users\\luca9\\testo.txt", get);
+}
+
+void test_file_locking_advanced() {
+	std::string file_name;
+	std::string get;
+	std::string mode;
+	int set = 0;
+
+	test_hazel_set("C:\\Users\\luca9\\testo.txt", "l", set);
+
+	test_hazel_get("C:\\Users\\luca9\\testo.txt", get);
+
+	test_hazel_set("C:\\Users\\luca9\\testo.txt", "l", set);
+
+	test_hazel_get("C:\\Users\\luca9\\testo.txt", get);
+
+	test_hazel_set("C:\\Users\\luca9\\testo.txt", "s", set);
+
+	test_hazel_get("C:\\Users\\luca9\\testo.txt", get);
+}
+
+int main()
+{
+	test_file_locking();
+	test_file_locking_advanced();
 
 	/* Fine */
 	return 0;
